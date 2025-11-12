8c58ca9393133d12ee0e8fcd32574c9fb1ef35a7
DavidowuCoding <155573245+DavidowuCoding@users.noreply.github.com>
2025-11-12 16:34:31 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: [
+      'be187786-c9d2-4452-90df-9872a8e3ef01-00-2nc5ikaoxanvy.picard.replit.dev',
+      '.replit.dev',
+      '.repl.co'
+    ]
+  }
 })
 
